e73b84e8cddc53c4408d7ce661b4c0f576a86fd5
Rico Schiekel <fire@downgra.de>
2010-07-29 19:01:49 +0200

    fix project setup for scalatest

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -15,7 +15,7 @@ class MainProject(info: ProjectInfo) extends DefaultProject(info) with AkkaProje
 
   //val junit = "junit"  % "junit"  % "4.7"  % "test"
   val specs = "org.scala-tools.testing"  % "specs_2.8.0.RC3"  % "1.6.5-SNAPSHOT"  % "test"
-  // val scalatest = "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC3" % "test"
+  val scalatest = "org.scalatest" % "scalatest" % "1.2-for-scala-2.8.0.RC3-SNAPSHOT" % "test"
 
   // Akka
   /** 
