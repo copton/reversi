833dd504301ef416d29a6c96957dea190f763a3f
Rico Schiekel <fire@downgra.de>
2010-07-30 16:09:38 +0200

    make Position comparable

--- src/main/java/reversi/Position.java
+++ src/main/java/reversi/Position.java
@@ -1,6 +1,6 @@
 package reversi;
 
-public class Position {
+public class Position implements Comparable<Position> {
     final public int x;
     final public int y;
 
@@ -22,5 +22,32 @@ public class Position {
     public boolean isValid()
     {
         return (x >= 0 && x < GameBoard.size && y >= 0 && y < GameBoard.size);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Position<%s, %s>", x, y);
+    }
+
+    @Override
+    public int hashCode() {
+        return 23 * x * y;
+    }
+
+    @Override
+    public int compareTo(Position other) {
+        if(x == other.x) {
+            return y - other.y;
+        } else {
+            return x - other.x;
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Position)) {
+            return false;
+        }
+        return compareTo((Position)obj) == 0;
     }
 }
