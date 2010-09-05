f2d1ddf0aab29be331cfc5d37b3d598959a5a703
Rico Schiekel <fire@downgra.de>
2010-09-05 17:04:55 +0200

    first attempt to make occupations (free, red, green) comparable

--- src/main/java/reversi/Color.java
+++ src/main/java/reversi/Color.java
@@ -1,16 +1,32 @@
 package reversi;
 
 public class Color extends Occupation {
-    protected Color() {}
-    public final static Color RED = new Color();
-    public final static Color GREEN = new Color();
+
+    public final static Color RED = new Color(100, "RED");
+    public final static Color GREEN = new Color(101, "GREEN");
+
+    protected Color(final int index, final String name) {
+        super(index, name);
+    }
 
     public static Color other(Color color) {
-        if (color == RED) return GREEN;
-        else return RED;
+        if (color.equals(RED)) {
+            return GREEN;
+        } else {
+            return RED;
+        }
     }
 
     public Color other() {
         return Color.other(this);
     }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (!(obj instanceof Color)) {
+            return false;
+        }
+        return compareTo((Color)obj) == 0;
+    }
+
 }
