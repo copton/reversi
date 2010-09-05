7c3d56b28168d3f4c0fd86861f00adfd0cf56c8c
Rico Schiekel <fire@downgra.de>
2010-09-05 16:34:27 +0200

    fix classpath and scala executable path problems

--- src/main/scala/game/PlayerProc.scala
+++ src/main/scala/game/PlayerProc.scala
@@ -4,6 +4,7 @@ import java.lang.{ProcessBuilder,Process}
 import se.scalablesolutions.akka.util.Logging
 import java.io.{InputStream, BufferedReader, InputStreamReader}
 import se.scalablesolutions.akka.actor.ActorRef
+import scala.util.Properties
 
 class StreamLogger(prefix: String, in: InputStream, logf: ((=>String) => Unit)) extends Thread {
 	override def run() {
@@ -19,9 +20,14 @@ class StreamLogger(prefix: String, in: InputStream, logf: ((=>String) => Unit)) 
 }
 
 class PlayerProc(val player: Player, val game: ActorRef, gamePort: Int) extends Thread with Logging {
-	val proc = new ProcessBuilder("/opt/scala/bin/scala", "-cp", PlayerProc.cp, "player.RunPlayer", player.port.toString, gamePort.toString).start()
-    val input = new StreamLogger("player" + player.port, proc.getInputStream(), log.info)
-	val output = new StreamLogger("player" + player.port, proc.getErrorStream(), log.error)
+  val proc = new ProcessBuilder(Properties.scalaCmd,
+                                "-cp",
+                                PlayerProc.cp,
+                                "player.RunPlayer",
+                                player.port.toString,
+                                gamePort.toString).start()
+  val input = new StreamLogger("player" + player.port, proc.getInputStream(), log.info)
+  val output = new StreamLogger("player" + player.port, proc.getErrorStream(), log.error)
 
   start()
 
@@ -36,5 +42,9 @@ class PlayerProc(val player: Player, val game: ActorRef, gamePort: Int) extends 
 }
 
 object PlayerProc {
-	val cp = "lib_managed/scala_2.8.0.RC3/compile/*:target/scala_2.8.0.RC3/classes:project/boot/scala-2.8.0.RC3/lib/scala-library.jar:target/scala_2.8.0.RC3/resources"
+  val SCALA_VERSION = Properties.scalaPropOrEmpty("version.number").replace(".final", "")
+  val cp = List("lib_managed/scala_%s/compile/*",
+                "target/scala_%s/classes",
+                "project/boot/scala-%s/lib/scala-library.jar",
+                "target/scala_%s/resources") map (_.format(SCALA_VERSION)) mkString ":"
 }
